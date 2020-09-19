fe7d759df370c8679c26665278a6f24f360674a1
TieWay59 <tieway59@foxmail.com>
2020-09-19 19:49:44 +0800

    简单格式化 rankList index.vue

--- src/views/rankList/index.vue
+++ src/views/rankList/index.vue
@@ -1,108 +1,111 @@
 <template>
   <div class="app-container">
-      <el-card class="bodybox ranklist">
-        <center>
-          <el-table
-            :data="tableData"
-            :fit="true"
-            style="width: 100%"
-            :row-class-name="tableRowClassName"
-          >
-            <el-table-column prop="rank" label="排名" width="160" align="center"></el-table-column>
-            <el-table-column label="用户名" width="200" align="center">
-              <template slot-scope="scope">
-                <router-link :to="'/userview?username=' + scope.row.username">{{ scope.row.username }}</router-link>
-              </template>
-            </el-table-column>
-            <el-table-column label="提交数" width="160" align="center">
-              <template slot-scope="scope">
-                <router-link :to="'/status?username=' + scope.row.username">{{ scope.row.submitted }}</router-link>
-              </template>
-            </el-table-column>
-            <el-table-column label="通过数" width="160" align="center">
-              <template slot-scope="scope">
-                <router-link :to="'/status?username=' + scope.row.username + '&AC=true'">{{ scope.row.accepted }}</router-link>
-              </template>
-            </el-table-column>
-            <el-table-column prop="score" label="积分" width="300" align="center"></el-table-column>
-          </el-table>
-        </center>
-        <center>
-          <div class="block">
-        <el-pagination
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="page.index"
-          :page-sizes="[10, 20, 50, 100]"
-          :page-size="page.size"
-          layout="total, sizes, prev, pager, next, jumper"
-          :total="page.total">
-        </el-pagination>
-      </div>
-    </center>
+    <el-card class="bodybox ranklist">
+      <center>
+        <el-table
+          :data="tableData"
+          :fit="true"
+          style="width: 100%"
+          :row-class-name="tableRowClassName"
+        >
+          <el-table-column prop="rank" label="排名" width="160" align="center" />
+          <el-table-column label="用户名" width="200" align="center">
+            <template slot-scope="scope">
+              <router-link :to="'/userview?username=' + scope.row.username">{{ scope.row.username }}</router-link>
+            </template>
+          </el-table-column>
+          <el-table-column label="提交数" width="160" align="center">
+            <template slot-scope="scope">
+              <router-link :to="'/status?username=' + scope.row.username">{{ scope.row.submitted }}</router-link>
+            </template>
+          </el-table-column>
+          <el-table-column label="通过数" width="160" align="center">
+            <template slot-scope="scope">
+              <router-link :to="'/status?username=' + scope.row.username + '&AC=true'">{{
+                scope.row.accepted
+              }}
+              </router-link>
+            </template>
+          </el-table-column>
+          <el-table-column prop="score" label="积分" width="300" align="center" />
+        </el-table>
+      </center>
+      <center>
+        <div class="block">
+          <el-pagination
+            :current-page="page.index"
+            :page-sizes="[10, 20, 50, 100]"
+            :page-size="page.size"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="page.total"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+          />
+        </div>
+      </center>
     </el-card>
   </div>
 </template>
 
 <script>
 import { getUserRanklist } from '@/api/user'
+
 export default {
-    name:'RankList',
-    data() {
-      return {
-        page:{
-            index:1,
-            size:20,
-            total:0,
-            query:''
-        },
-        tableData: [],
-      };
-    },
-    methods:{
-        handleSizeChange(val) {
-          this.page.size = val
-          this.flushUserList()
-        },
-        handleCurrentChange(val) {
-          this.page.index = val
-          this.flushUserList()
-        },
-        flushUserList: function() {
-          getUserRanklist(this.page.index, this.page.size).then( res => {
-            this.tableData = res.data.page.content
-            this.tableData.unshift(res.data.userself)
-            this.page.total = res.data.page.totalElements
-          }).catch( err => {
-            this.$message({
-              type: 'error',
-              message: '获取列表失败'
-            })
-          })
-        },
-        tableRowClassName({ row, rowIndex }) {
-          if (row.username == this.$store.state.user.username) {
-            return 'self-row'
-          }
-          return ''
-        },
-    },
-    mounted(){
-        this.flushUserList()
+  name: 'RankList',
+  data() {
+    return {
+      page: {
+        index: 1,
+        size: 20,
+        total: 0,
+        query: ''
+      },
+      tableData: []
     }
-};
+  },
+  mounted() {
+    this.flushUserList()
+  },
+  methods: {
+    handleSizeChange(val) {
+      this.page.size = val
+      this.flushUserList()
+    },
+    handleCurrentChange(val) {
+      this.page.index = val
+      this.flushUserList()
+    },
+    flushUserList: function() {
+      getUserRanklist(this.page.index, this.page.size).then(res => {
+        this.tableData = res.data.page.content
+        this.tableData.unshift(res.data.userself)
+        this.page.total = res.data.page.totalElements
+      }).catch(() => {
+        this.$message({
+          type: 'error',
+          message: '获取列表失败'
+        })
+      })
+    },
+    tableRowClassName({ row, rowIndex }) {
+      if (row.username === this.$store.state.user.username) {
+        return 'self-row'
+      }
+      return ''
+    }
+  }
+}
 </script>
 <style lang="scss">
-.bodybox{
+.bodybox {
   margin-top: 20px;
 }
 
-.el-col{
-    border: 1px solid transparent; 
+.el-col {
+  border: 1px solid transparent;
 }
 
-
 .ranklist .el-table .self-row {
-    background: #d6e9ca;
+  background: #d6e9ca;
 }
 </style>
